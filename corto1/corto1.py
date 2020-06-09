c302697979a77768dca86399d86651a4497194b6
deydrums <3000784390101@ingenieria.usac.edu.gt>
2020-06-09 12:34:10 -0600

    Algoritmo con archivo de texto

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -5,14 +5,25 @@ def coll(numero):                       #FUNCION PARA EL ALGORITMO
     lista = []                          #SE CREA UNA LISTA
     while numero != 1:                  #CICLO MIENTRAS NUMERO NO SEA 1
         if numero % 2 == 0:             #SI EL NUMERO ES PAR
-            lista.append(numero)
+            lista.append(int(numero))
             numero = numero / 2
         else:                           #SI EL NUMERO ES IMPAR
-            lista.append(numero)
+            lista.append(int(numero))
             numero = (numero * 3) + 1
         if numero == 1:                 #SI EL NUMERO ES 1
-            lista.append(numero)
+            lista.append(int(numero))
     return(lista)
 
-for x in range (2 , 883):               #RECORRER LOS NUMEROS DESDE EL 2 HASTA 883-1(882)
-    print(coll(x))
+
+def fileWrite(fileName = 'collatz.txt'):
+    archivo = open(fileName,'w') 
+    archivo.write('Sobreescribiendo archivo...\n')
+    print('Espere, sobreescribiendo el archivo...')
+    for x in range (2 , 883):              
+        print(coll(x))
+        archivo.write(str(coll(x)))
+        archivo.write('\n')
+    archivo.close() #Siempre cerrar el archivo al finalizar la escritura
+    print('Append finalizado')
+
+fileWrite()
